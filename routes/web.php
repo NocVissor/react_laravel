805fc6180d12726b0f3b88319cb0cb19ebac1d44
kavenduro <plyron@mail.ru>
2021-12-26 15:46:32 +0300

    добавление react, и некоторых дополнительных библиотек

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('react');
 });
+
+Route::get('/{catchall?}', function () {
+    return response()->view('react');
+})->where('catchall', '(.*)');
